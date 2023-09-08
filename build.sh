2771aa3d62640f57352f647b60b745f783061233
James <jamesit@d5r.ru>
2023-09-08 19:21:55 +0300

    fixing shit

--- build.sh
+++ build.sh
@@ -3,11 +3,6 @@
 cd build
 ./build.sh
 ./build.backend.docker.sh
-PROXY_ID=$(docker ps|grep proxy| awk '{print $1}')
-docker stop $PROXY_ID
-docker rm $PROXY_ID
-cd install/docker
-docker-compose -f /home/james/DocSpace/build/install/docker/james.yml up -d
-cd /home/james/DocSpace
+cd ..
 yarn build
 
